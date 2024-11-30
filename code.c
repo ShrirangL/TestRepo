1df80d0159b21093056f4fe93c3870d8615c83bd
Shrirang <N/A>
2024-11-30 21:20:32 +0000

    added void

--- code.c
+++ code.c
@@ -1,7 +1,11 @@
 // file: main.c
 #include <stdio.h>
 
+<<<<<<< HEAD
 int main(int argc, char* argv[]) {
+=======
+int main(void) {
+>>>>>>> c708c33 (added arg void in main)
     //Dummy comment changed
     printf("Dummy C program");
     //new comment added by user 1
