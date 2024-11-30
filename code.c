bcab58e77934298b2e6d644a6555489edcbe560c
Shrirang <N/A>
2024-11-30 21:23:11 +0000

    added void after resolving file

--- code.c
+++ code.c
@@ -1,11 +1,7 @@
 // file: main.c
 #include <stdio.h>
 
-<<<<<<< HEAD
-int main(int argc, char* argv[]) {
-=======
 int main(void) {
->>>>>>> c708c33 (added arg void in main)
     //Dummy comment changed
     printf("Dummy C program");
     //new comment added by user 1
